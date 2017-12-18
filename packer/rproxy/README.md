4e9f33c9605f3bfbc0076490f3e2c7bc69531d44
nmaupu <nmaupu@gmail.com>
2017-12-18 01:01:47 +0100

    Adding RP LB for future ingress controllers
    - Reverse proxy with nginx
    - Ingress controllers have to be deployed using configrable port
    - Makefile is done so we can have multiple ingress controllers

---
+++ packer/rproxy/README.md
@@ -0,0 +1,7 @@
+# Building
+
+Building is done using the following commands :
+```
+PROJECT_ID=<project_id> IMAGE_NAME_PREFIX=rproxy-pub  INGRESS_CONTROLLER_PORT=30080
+PROJECT_ID=<project_id> IMAGE_NAME_PREFIX=rproxy-priv INGRESS_CONTROLLER_PORT=30081
+```
---
+++ packer/rproxy/resources/update-kube-nodes.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+NODES=$(gcloud compute instances list --filter="gke-*" --format=json | jq -r '.[].networkInterfaces | .[].networkIP')
+PORT=@@PORT@@
+FILE=/etc/nginx/kube-nodes.inc
+
+echo -n "" > ${FILE}
+for node in ${NODES}; do
+  echo "server ${node}:${PORT};" >> ${FILE}
+done
+
+systemctl reload nginx
