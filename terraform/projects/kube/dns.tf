0436f9af047329cb5b669a7e7a12fd4dd75f663a
nmaupu <nmaupu@gmail.com>
2018-01-07 18:37:18 +0100

    Added A records to rproxy machines

--- terraform/projects/kube/dns.tf
+++ terraform/projects/kube/dns.tf
@@ -1,4 +1,4 @@
-module "rproxy-dns-record" {
+module "rproxy-pub-dns-record-A" {
   source       = "../../modules/dns/record_set"
   name         = "*.${data.terraform_remote_state.projects.kube.dns_name}"
   type         = "A"
@@ -12,7 +12,7 @@ module "rproxy-dns-record" {
   project_id = "${data.terraform_remote_state.projects.kube.project_id}"
 }
 
-module "rproxy-priv-dns-record" {
+module "rproxy-priv-dns-record-A" {
   source       = "../../modules/dns/record_set"
   name         = "*.${data.terraform_remote_state.projects.kube.priv_dns_name}"
   type         = "A"
@@ -21,6 +21,34 @@ module "rproxy-priv-dns-record" {
 
   rrdatas = [
     "${module.rproxy-priv-instance.nat_ip}",
+  ]
+
+  project_id = "${data.terraform_remote_state.projects.kube.project_id}"
+}
+
+module "rproxy-pub-dns-record-A-internal" {
+  source       = "../../modules/dns/record_set"
+  name         = "rproxy.${data.terraform_remote_state.projects.kube.dns_name}"
+  type         = "A"
+  ttl          = "3600"
+  managed_zone = "${data.terraform_remote_state.projects.kube.managed_zone}"
+
+  rrdatas = [
+    "${module.rproxy-pub-instance.internal_ip}",
+  ]
+
+  project_id = "${data.terraform_remote_state.projects.kube.project_id}"
+}
+
+module "rproxy-priv-dns-record-A-internal" {
+  source       = "../../modules/dns/record_set"
+  name         = "rproxy.${data.terraform_remote_state.projects.kube.priv_dns_name}"
+  type         = "A"
+  ttl          = "3600"
+  managed_zone = "${data.terraform_remote_state.projects.kube.priv_managed_zone}"
+
+  rrdatas = [
+    "${module.rproxy-priv-instance.internal_ip}",
   ]
 
   project_id = "${data.terraform_remote_state.projects.kube.project_id}"
