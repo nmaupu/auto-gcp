838439fa5038fd6dcef9fbbd53ee9e5b166f0919
nmaupu <nmaupu@gmail.com>
2018-03-10 19:54:14 +0100

    Making project initialization generic

--- terraform/projects/init/README.md
+++ terraform/projects/init/README.md
@@ -8,5 +8,7 @@ The project content has to be deleted first and only then, the project.
 TF_VAR_kube_dns_name = "pub.example.com." TF_VAR_kube_priv_dns_name = "priv.example.com." make
 ```
 
+This activate DNS api and sometimes, api has not enough time to activate before zones are created and thus terraform fails with an error. Re-run and it should work as expected !
+
 To be able to use those domain, you have to register output NS into your registrar.
 Beware: when refreshing zone (recreating for example), NS can change !
--- terraform/projects/init/kube.tf
+++ terraform/projects/init/kube.tf
@@ -1,51 +1,50 @@
 ##
 # Creating project
 ##
-# TODO - when quota is ok again, make it generic
-#module "kube" {
-#  source = "../../modules/project"
-#  project_name = "${var.kube_project_name}"
-#  org_id = "${var.org_id}"
-#  billing_account = "${var.billing_account}"
-#}
-resource "google_project" "kube" {
-  name            = "kube"
-  project_id      = "kube-802de212"
+module "kube" {
+  source = "../../modules/project"
+  project_name = "${var.kube_project_name}"
+  org_id = "${var.org_id}"
   billing_account = "${var.billing_account}"
-  org_id          = "${var.org_id}"
 }
 
 module "kube_service" {
   source     = "../../modules/project_service"
-  project_id = "${google_project.kube.project_id}"
+  project_id = "${module.kube.project_id}"
   service    = "compute.googleapis.com"
+}
+
+module "dns_service" {
+  source      = "../../modules/project_service"
+  project_id = "${module.kube.project_id}"
+  service   = "dns.googleapis.com"
 }
 
 module "kube_sshkeys" {
   source      = "../../modules/sshkeys"
   credentials = "${var.credentials}"
-  project     = "${google_project.kube.project_id}"
+  project     = "${module.kube.project_id}"
 }
 
 module "kube_managed_zone" {
   source     = "../../modules/dns/managed_zone"
   name       = "${var.kube_zone_name}"
   dns_name   = "${var.kube_dns_name}"
-  project_id = "${google_project.kube.project_id}"
+  project_id = "${module.kube.project_id}"
 }
 
 module "kube_priv_managed_zone" {
   source     = "../../modules/dns/managed_zone"
   name       = "${var.kube_priv_zone_name}"
   dns_name   = "${var.kube_priv_dns_name}"
-  project_id = "${google_project.kube.project_id}"
+  project_id = "${module.kube.project_id}"
 }
 
 #
 ## Outputs
 #
 output "kube.project_id" {
-  value = "${google_project.kube.project_id}"
+  value = "${module.kube.project_id}"
 }
 
 output "kube.managed_zone" {
