472955a873f1821cca6de36f05e97c5027e4969d
nmaupu <nmaupu@gmail.com>
2018-03-16 02:17:37 +0100

    Removing test cert option to LE

--- packer/rproxy/resources/ingress-add.sh
+++ packer/rproxy/resources/ingress-add.sh
@@ -6,5 +6,5 @@ for dom in $(echo ${DOMAINS} | tr -s "," " "); do
   CMD_DOMS="${CMD_DOMS} -d ${dom}"
 done
 
-sudo certbot certonly --test-cert --agree-tos --register-unsafely-without-email -n --standalone --expand --preferred-challenges http ${CMD_DOMS}
+sudo certbot certonly --agree-tos --register-unsafely-without-email -n --standalone --expand --preferred-challenges http ${CMD_DOMS}
 sudo systemctl reload nginx
