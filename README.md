be9398d98068d3e63365b68a47f6a2b2b0500cc4
Nicolas Maupu <nmaupu@gmail.com>
2025-03-15 18:11:25 +0100

    Cleaning + new nuki-logger

---
+++ README.md
@@ -0,0 +1,1 @@
+See bootstrap-gce/Makefile to have a better idea on how to start (create credentials).
--- terraform/projects/init/dns.tf
+++ terraform/projects/init/dns.tf
@@ -143,31 +143,31 @@ module "home_root_records" {
   ]
 }
 
-module "nas_home_root_records" {
-  source       = "../../modules/dns/record_set"
-  project_id   = module.kube.project_id
-  managed_zone = module.kube_priv1_managed_zone.name
-  name         = "nas.home.${module.kube_priv1_managed_zone.dns_name}"
-
-  type = "CNAME"
-  ttl  = "86400"
-
-  rrdatas = [
-    "home.${module.kube_priv1_managed_zone.dns_name}",
-  ]
-}
-
-module "hass_home_root_records" {
-  source       = "../../modules/dns/record_set"
-  project_id   = module.kube.project_id
-  managed_zone = module.kube_priv1_managed_zone.name
-  name         = "hass.home.${module.kube_priv1_managed_zone.dns_name}"
-
-  type = "CNAME"
-  ttl  = "86400"
-
-  rrdatas = [
-    "home.${module.kube_priv1_managed_zone.dns_name}",
-  ]
-}
+# module "nas_home_root_records" {
+#   source       = "../../modules/dns/record_set"
+#   project_id   = module.kube.project_id
+#   managed_zone = module.kube_priv1_managed_zone.name
+#   name         = "nas.home.${module.kube_priv1_managed_zone.dns_name}"
+#
+#   type = "CNAME"
+#   ttl  = "86400"
+#
+#   rrdatas = [
+#     "home.${module.kube_priv1_managed_zone.dns_name}",
+#   ]
+# }
+#
+# module "hass_home_root_records" {
+#   source       = "../../modules/dns/record_set"
+#   project_id   = module.kube.project_id
+#   managed_zone = module.kube_priv1_managed_zone.name
+#   name         = "hass.home.${module.kube_priv1_managed_zone.dns_name}"
+#
+#   type = "CNAME"
+#   ttl  = "86400"
+#
+#   rrdatas = [
+#     "home.${module.kube_priv1_managed_zone.dns_name}",
+#   ]
+# }
 
